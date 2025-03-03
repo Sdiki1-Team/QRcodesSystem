64d5fc1dae907a17296169006efe042d8960718c
sdiki1 <volodymyrbuharin@yandex.ru>
2025-03-03 22:15:56 +0300

    Fixed QR code

--- main_app/admin.py
+++ main_app/admin.py
@@ -78,7 +78,7 @@ class ObjectAdmin(admin.ModelAdmin):
         obj = self.get_object(request, object_id)
         if obj and obj.qr_code:
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-            qr.add_data(obj.qr_code)
+            qr.add_data('https://'+obj.qr_code)
             qr.make(fit=True)
             img = qr.make_image(fill='black', back_color='white')
             buffered = BytesIO()
